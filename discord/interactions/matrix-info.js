78a17b2de9e80ceb567c518e648972ef630914d9
Cadence Ember <cadence@disroot.org>
2024-08-28 00:17:54 +1200

    Update formatting of matrix info command

--- discord/interactions/matrix-info.js
+++ discord/interactions/matrix-info.js
@@ -7,7 +7,8 @@ const {discord, sync, db, select, from} = require("../../passthrough")
 const api = sync.require("../../matrix/api")
 
 /** @param {DiscordTypes.APIContextMenuGuildInteraction} interaction */
-async function interact({id, token, data}) {
+/** @param {DiscordTypes.APIMessageApplicationCommandGuildInteraction} interaction */
+async function interact({id, token, guild_id, channel, data}) {
 	const message = from("event_message").join("message_channel", "message_id").join("channel_room", "channel_id")
 		.select("name", "nick", "source", "room_id", "event_id").where({message_id: data.target_id}).get()
 
@@ -21,12 +22,15 @@ async function interact({id, token, data}) {
 		})
 	}
 
+	const idInfo = `\n-# Room ID: \`${message.room_id}\`\n-# Event ID: \`${message.event_id}\``
+
 	if (message.source === 1) { // from Discord
+		const userID = data.resolved.messages[data.target_id].author.id
 		return discord.snow.interaction.createInteractionResponse(id, token, {
 			type: DiscordTypes.InteractionResponseType.ChannelMessageWithSource,
 			data: {
-				content: `This message was bridged to [${message.nick || message.name}](<https://matrix.to/#/${message.room_id}/${message.event_id}>) on Matrix.`
-					+ `\n-# Room ID: \`${message.room_id}\`\n-# Event ID: \`${message.event_id}\``,
+				content: `Bridged <@${userID}> https://discord.com/channels/${guild_id}/${channel.id}/${data.target_id} on Discord to [${message.nick || message.name}](<https://matrix.to/#/${message.room_id}/${message.event_id}>) on Matrix.`
+					+ idInfo,
 				flags: DiscordTypes.MessageFlags.Ephemeral
 			}
 		})
@@ -37,9 +41,8 @@ async function interact({id, token, data}) {
 	return discord.snow.interaction.createInteractionResponse(id, token, {
 		type: DiscordTypes.InteractionResponseType.ChannelMessageWithSource,
 		data: {
-			content: `This message was bridged from [${message.nick || message.name}](<https://matrix.to/#/${message.room_id}/${message.event_id}>) on Matrix.`
-				+ `\nIt was originally sent by [${event.sender}](<https://matrix.to/#/${event.sender}>).`
-				+ `\n-# Room ID: \`${message.room_id}\`\n-# Event ID: \`${message.event_id}\``,
+			content: `Bridged [${event.sender}](<https://matrix.to/#/${event.sender}>)'s message in [${message.nick || message.name}](<https://matrix.to/#/${message.room_id}/${message.event_id}>) on Matrix to https://discord.com/channels/${guild_id}/${channel.id}/${data.target_id} on Discord.`
+				+ idInfo,
 			flags: DiscordTypes.MessageFlags.Ephemeral
 		}
 	})
