dbfa9d0f2b797de442b9654ca28b10240d405604
Cadence Ember <cadence@disroot.org>
2026-02-10 16:42:02 +1300

    Sync PK member profile on first message
    First time a PK member sends a message in the channel, Discord sends a
    MESSAGE_UPDATE with the proper avatar data for them. OOYE's speedbump
    means sending this message will actually take the edit message path.
    The edit message path previously did not force a profile sync. This is
    why the Matrix profile did always show up after their second message,
    because that message was not updated and took the send path.

--- src/d2m/actions/edit-message.js
+++ src/d2m/actions/edit-message.js
@@ -30,7 +30,7 @@ async function editMessage(message, guild, row) {
 	if (row && row.speedbump_webhook_id === message.webhook_id) {
 		// Handle the PluralKit public instance
 		if (row.speedbump_id === "466378653216014359") {
-			senderMxid = await registerPkUser.syncUser(message.id, message.author, roomID, false)
+			senderMxid = await registerPkUser.syncUser(message.id, message.author, roomID, true)
 		}
 	}
 
