59b5e05b339b4ebdefbb6e4b6b88547aebe4b864
Cadence Ember <cadence@disroot.org>
2026-09-24 13:18:52 +1200

    Make m->d Klipy links not produce error

--- src/d2m/converters/message-to-event.js
+++ src/d2m/converters/message-to-event.js
@@ -785,7 +785,7 @@ async function messageToEvent(message, guild, options = {}, di) {
 	// Send Klipy GIFs in customised form
 	let isKlipyGIF = false
 	let isOnlyKlipyGIF = false
-	if (message.embeds?.length === 1 && message.embeds[0].provider?.name === "Klipy" && message.embeds[0].video?.url) {
+	if (message.content && message.embeds?.length === 1 && message.embeds[0].provider?.name === "Klipy" && message.embeds[0].video?.url) {
 		isKlipyGIF = true
 		if (message.content.match(/^https?:\/\/klipy\.com[^ \n]+$/)) {
 			isOnlyKlipyGIF = true
